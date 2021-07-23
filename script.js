47271c9aaf05d02bfd0ad7e5f7da7d64e5a1ac14
Austin Chau <austin.chau1@gmail.com>
2021-07-22 20:00:04 -0400

    changed from constructor to class

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let cacheLibrary = localStorage;
 let id = 0;
 let cacheAvailable = 1;
 
-function Book(id,title,author,pages,read) {
+class Book{
+constructor(id,title,author,pages,read) {
 this.id = id;
 this.title = title;
 this.author = author;
 this.pages = pages;
 this.read = read;
+}
 }
 
 function addBooktoLibrary(e){
